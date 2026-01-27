635779451282a951d04254b8ce557c74276bd27d
hzlloni <130718048+hzlloni@users.noreply.github.com>
2026-01-27 10:26:52 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.10 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -0,0 +1,15 @@
+def solution(n):
+    
+    # return ('수박'*n)[:n]
+
+    answer = ''
+    
+    for i in range(n):
+        if i % 2 == 0:
+            answer += '수'
+        else:
+            answer += '박'
+    
+    return answer
+
+
