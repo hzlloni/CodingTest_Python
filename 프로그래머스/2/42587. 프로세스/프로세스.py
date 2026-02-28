b558dec9917ef42bb2914e3cb1a7fbe1649acfc9
hzlloni <130718048+hzlloni@users.noreply.github.com>
2026-02-28 14:09:58 +0900

    Update 프로세스.py

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -19,15 +19,18 @@ def solution(priorities, location):
 from collections import deque
 
 def solution(priorities, location):
-    queue = deque((p, i) for i, p in enumerate(priorities))
-    order = 0
+    answer = 0
+    queue = deque()
+
+    for i in range(len(priorities)):
+        queue.append((priorities[i], i))
 
     while queue:
-        cur_p, cur_i = queue.popleft()
+        cur = queue.popleft()
 
-        if any(p > cur_p for p, _ in queue):
-            queue.append((cur_p, cur_i))
+        if queue and cur[0] < max(x[0] for x in queue):
+            queue.append(cur)
         else:
-            order += 1
-            if cur_i == location:
-                return order
+            answer += 1
+            if cur[1] == location:
+                return answer
