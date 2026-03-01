3966152dc0367be84ee2e7142ac19852b3505640
hzlloni <130718048+hzlloni@users.noreply.github.com>
2026-03-01 15:05:26 +0900

    [level 2] Title: 피보나치 수, Time: 147.07 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,16 @@
+def solution(n):
+    a = 1
+    b = 1
+    if n == 1 or n == 2:
+        return 1
+    
+    for i in range(1, n):
+        a, b = b, b+a
+        
+    return a % 1234567
+
+# def solution(n):
+#     if n == 1 or n == 2:
+#         return 1
+#     else:
+#         return solution(n-2) + solution(n-1)
